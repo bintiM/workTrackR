054fe564242faab9b2c664bc8167a6a94c2f2795
bintiM <marc@bintinger.at>
2015-08-25 19:59:24 +0200

    Initial Commit

---
+++ workTrackRTests/workTrackRTests.swift
@@ -0,0 +1,36 @@
+//
+//  workTrackRTests.swift
+//  workTrackRTests
+//
+//  Created by Marc Bintinger on 25.08.15.
+//  Copyright (c) 2015 Marc Bintinger. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class workTrackRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
